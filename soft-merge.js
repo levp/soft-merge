53b0b73104f95759fcff3f25b196baaef6da4a22
levp <levi2p@gmail.com>
2016-10-12 09:29:09 +0200

    Initial commit.

---
+++ soft-merge.js
@@ -0,0 +1,65 @@
+'use strict';
+
+module.exports = softMerge;
+
+/**
+ * Performs a 'soft merge' by copying properties from sourceObject unto targetObject recursively.
+ * This function differs from traditional merge behavior in that it will merge arrays, if both targetObject and
+ * sourceObject hold an array for a given property.
+ * @param targetObject {object} The object unto which properties will be copied.
+ * @param sourceObject {object} The object from which properties will be read.
+ */
+function softMerge(targetObject, sourceObject) {
+	Object.keys(sourceObject).forEach(key => {
+		const value = sourceObject[key];
+
+		if (!targetObject.hasOwnProperty(key)) {
+			// target object doesn't have such a key at all,
+			// so just copy the value from the source object
+			targetObject[key] = value;
+			return;
+		}
+
+		if (isPrimitive(value)) {
+			// the source object value is primitive, which
+			// means there's no point of any further actions
+			// besides simply overriding the target object value
+			targetObject[key] = value;
+			return;
+		}
+
+		const originalValue = targetObject[key];
+		if (isPrimitive(originalValue)) {
+			// the target object value is primitive, so just
+			// override it with the source value
+			targetObject[key] = value;
+			return;
+		}
+
+		// if this point has been reached then both 'originalValue'
+		// and 'value' are objects, requiring a recursive merger
+		softMerge(originalValue, value);
+	});
+}
+
+/**
+ *  Returns true if the specified value is one of the following:
+ *  1. undefined
+ *  2. null
+ *  3. primitive number
+ *  4. primitive boolean
+ *  5. primitive string
+ *
+ *  otherwise returns false.
+ */
+function isPrimitive(value) {
+	if (!value) {
+		// catches undefined and null
+		return true;
+		// this also catches a few other values, but all of
+		// them would be caught anyway in the next check
+		// (except document.all, but that's just silly)
+	}
+	const type = typeof value;
+	return type === 'number' || type === 'boolean' || type === 'string';
+}
